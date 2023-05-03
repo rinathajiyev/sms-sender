6705bd810a50e2a5efd01e48eb4c3c6272920709
rinat18 <haciyev.rinat.2002@gmail.com>
2023-05-03 16:19:59 +0400

    Phone number validation added

--- SmsSender/src/main/java/com/company/impl/TwilioSmsSender.java
+++ SmsSender/src/main/java/com/company/impl/TwilioSmsSender.java
@@ -3,6 +3,7 @@ package com.company.impl;
 import com.company.config.*;
 import com.company.inter.*;
 import com.company.sms.*;
+import com.google.i18n.phonenumbers.*;
 import com.twilio.rest.api.v2010.account.*;
 import com.twilio.type.*;
 import org.slf4j.*;
@@ -24,11 +25,28 @@ public class TwilioSmsSender implements SmsSender {
 
     @Override
     public void sendSms(SmsRequest smsRequest) {
-        PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
-        PhoneNumber from = new PhoneNumber(twilioConfiguration.getTrialNumber());
-        String message = smsRequest.getMessage();
-        MessageCreator creator = Message.creator(to, from, message);
-        creator.create();
-        LOGGER.info("Send sms {}", smsRequest);
+        if(isValidPhoneNumber(smsRequest.getPhoneNumber())) {
+            PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
+            PhoneNumber from = new PhoneNumber(twilioConfiguration.getTrialNumber());
+            String message = smsRequest.getMessage();
+            MessageCreator creator = Message.creator(to, from, message);
+            creator.create();
+            LOGGER.info("Send sms {}", smsRequest);
+        } else{
+            System.out.println(new IllegalArgumentException("Format of the phone number is incorrect!"));
+        }
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber){
+        PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+        boolean isValid = false;
+        try{
+            Phonenumber.PhoneNumber phoneNumberObject = phoneNumberUtil.parse(phoneNumber, "AZE");
+            isValid = phoneNumberUtil.isValidNumber(phoneNumberObject);
+        }catch (NumberParseException e){
+            System.err.println("NumberParseException: " + e.toString());
+        }
+
+        return isValid;
     }
 }
